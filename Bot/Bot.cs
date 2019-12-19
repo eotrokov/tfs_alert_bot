5abf5221ac7716d1418241c465a86243f08f34aa
evgeny otrokov <otrokov@me.com>
2019-12-19 21:02:31 +0300

    init bot

---
+++ Bot/Bot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace Bot
+{
+    public class Bot
+    {
+        public TelegramBotClient BotClient;
+
+        private Bot()
+        {
+            Environment.SetEnvironmentVariable("Token", "1014817526:AAGC3EarTPI0d2HL8UTQ9mCnh2krzEf3UJg");
+            BotClient = new TelegramBotClient(Environment.GetEnvironmentVariable("Token"));
+            BotClient.OnMessage += BotClientOnOnMessage;
+        }
+
+        private void BotClientOnOnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Chat.Id);
+            Console.WriteLine(e.Message.Text);
+            BotClient.SendTextMessageAsync(e.Message.Chat.Id,
+                $"привет, вот ссылка {Environment.GetEnvironmentVariable("HOST")}/bot{e.Message.Chat.Id}",
+                ParseMode.Markdown);
+        }
+
+
+        private static Bot _instance;
+
+        public static Bot Instance => _instance ??= new Bot();
+    }
+}
---
+++ WebApi/Controllers/WebHookController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+using Telegram.Bot.Types.Enums;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebHookController : ControllerBase
+    {
+        
+        private readonly ILogger<WebHookController> _logger;
+
+        public WebHookController(ILogger<WebHookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Get(string id, [FromBody]Data data)
+        {
+            Bot.Bot.Instance.BotClient.SendTextMessageAsync(id, data.Message.Markdown, ParseMode.Markdown);
+            return Ok(id);
+        }
+    }
+}
